dda232142cf609261858063cab4a3091c40b5c4e
monicacimirro <monicacimirro@gmail.com>
2024-12-14 14:46:50 -0300

    Update Menu.py

--- code/Menu.py
+++ code/Menu.py
@@ -23,6 +23,7 @@ class Menu:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mônica Scremin Cimirro RU 250787", C_ORANGE, (0, 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
